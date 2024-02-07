053cbea8a91560b710431a3ebc0ff6f9904296ee
kanchanlata kujur <kanchanlatakujur41@gmail.com>
2024-02-07 17:41:36 +0530

    second and good way to connected database

--- chai-backend/src/index.js
+++ chai-backend/src/index.js
@@ -1,35 +1,24 @@
-// require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
-import express from 'express';
-import mongoose from 'mongoose';
-import {DB_NAME} from './constants.js'
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-dotenv.config();
+dotenv.config({path: './env'})
 
-const app  =  express();
 
-(async ()=> {
-    console.log("hi");
-    try {
-        console.log("hi again");
-         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-      
-        app.on("error", (error)=>{
-            console.log("ERROR:",error)
-            throw error
-       
-        
-        })
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-        }) 
-    } catch (error) {
-        console.log("ERROR:", error)
+    })
+
+    app.on("error", (error)=>{
+        console.log("ERROR:",error)
         throw error
-
-        
-    }
-
-})();
+    })
+    
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!! ", err);
+})
 
